d2a16dead7e0cb3e306ca04371307f67b72a97ba
jiangfeng <1546844168@qq.com>
2018-11-03 19:21:13 +0800

    学习JVM

--- src/chapterEight/Hello.java
+++ src/chapterEight/Hello.java
@@ -1,18 +1,40 @@
 package chapterEight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author 潇潇暮雨
  * @create 2018-11-03   13:24
  */
 public class Hello {
     public static void main(String[] args) {
-        String name = "林加铭";
-        Hello.sayHello();
+        Hello hello = new Hello();
+        //        String name = "林加铭";
+//        Hello.sayHello();
 //        System.gc();
+//        int calc =hello.calc();
+//        System.out.println(calc);
+        hello.sayHello();
     }
 
     public static void sayHello() {
-        int i = 0;
+        final int i = 0;
         System.out.println("hello");
+    }
+
+    public int calc() {
+        Object o = new Object();
+        int a = 100;
+        int b = 200;
+        int c = 300;
+        return (a + b) * c;
+    }
+
+    public void coll() {
+        List list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        System.out.println(list);
     }
 }
---
+++ src/chapterTen/Generic.java
@@ -0,0 +1,8 @@
+package chapterTen;
+
+/**
+ * @author 潇潇暮雨
+ * @create 2018-11-03   19:20
+ */
+public class Generic {
+}
