229175a96a2bdaa56eee77fffdc09d8137a46a5b
jiangfeng <1546844168@qq.com>
2018-11-06 17:02:32 +0800

    学习JVM

---
+++ src/JVM_video/Demo4.java
@@ -0,0 +1,13 @@
+package JVM_video;
+
+/**
+ * @author 潇潇暮雨
+ * @create 2018-11-06   16:54
+ */
+public class Demo4 {
+    public static void main(String[] args) {
+        while (true) {
+            int[] arr = new int[1024 * 1024];
+        }
+    }
+}
