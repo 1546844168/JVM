d89eb23d3b9bf6700105c0c5ab371d455edcb972
jiangfeng <1546844168@qq.com>
2018-11-07 10:32:06 +0800

    学习JVM

--- src/JVM_video/Demo4.java
+++ src/JVM_video/Demo4.java
@@ -1,13 +1,26 @@
 package JVM_video;
 
+import java.util.ArrayList;
+
 /**
  * @author 潇潇暮雨
  * @create 2018-11-06   16:54
  */
 public class Demo4 {
+    byte[] arr = new byte[1 * 1024 * 1024];// 1MB
+
     public static void main(String[] args) {
+        ArrayList<Demo4> list = new ArrayList<>();
+        int count = 0;
+
+//        try {
         while (true) {
-            int[] arr = new int[1024 * 1024];
+            count++;
+            list.add(new Demo4());
+//            }
+//        } catch (Throwable e) {
+//            System.out.println(count);
         }
+
     }
 }
