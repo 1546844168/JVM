6db77ecc5457d2c7a5419fb48b431a405309fd6f
江峰 <jiangfeng@smarchit.com>
2019-04-21 09:56:33 +0800

    学习JVM

--- src/JVM_video/Demo4.java
+++ src/JVM_video/Demo4.java
@@ -12,14 +12,14 @@ public class Demo4 {
     public static void main(String[] args) {
         ArrayList<Demo4> list = new ArrayList<>();
         int count = 0;
-
-//        try {
-        while (true) {
-            count++;
-            list.add(new Demo4());
-//            }
-//        } catch (Throwable e) {
-//            System.out.println(count);
+        try {
+            while (true) {
+                count++;
+                System.out.println(count);
+                list.add(new Demo4());
+            }
+        } catch (Throwable e) {  // 内存溢出是error。
+            System.out.println(count);
         }
 
     }
